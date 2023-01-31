246a32bdf51127edfe499e9ae87ca2e146f3859a
naderbennour <naderbennour@outlook.com>
2023-01-31 11:22:41 +0100

    updated docs

--- main.go
+++ main.go
@@ -56,7 +56,6 @@ type newContractResponse struct {
 // @license.name Apache 2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host localhost:8080
 // @BasePath /
 
 func main() {
